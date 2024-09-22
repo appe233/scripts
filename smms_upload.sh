382fbc5723c2638142865aaca4f0fce70094b704
appe233 <89209249+appe233@users.noreply.github.com>
2024-09-22 09:38:35 +0800

    Update smms_upload.sh

--- smms_upload.sh
+++ smms_upload.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-# Use it with Typora
-API_KEY="" # Enter the API key here.
+#!/bin/zsh
+# Use it with Typora + smms
+# Rename pictures' names to be their hash
+API_KEY="" # Enter your API key here
 for image in $@
 do
-    OUT=$(curl -s -X POST -H "Authorization: $API_KEY" -F "smfile=@$image" https://sm.ms/api/v2/upload)
-    echo echo $(echo "$OUT" | jq -r ".message")
-    echo $(echo "$OUT" | jq -r ".data.url")
+    HASH_NAME=$(shasum -b -a 256 $image | awk '{ print $1 }')
+    SUFFIX=$(basename $image | grep -o "\.[a-z]\+")
+    cp $image "/Users/appe/$HASH_NAME$SUFFIX"
+    OUT=$(curl -s -X POST -H "Authorization: $API_KEY" -F "smfile=@/Users/appe/$HASH_NAME$SUFFIX" https://smms.app/api/v2/upload) && rm /Users/appe/$HASH_NAME$SUFFIX
+    # echo $(echo "$OUT" | jq -r ".message")
+    echo $(echo "$OUT" | /opt/homebrew/bin/jq -r ".data.url")
 done
