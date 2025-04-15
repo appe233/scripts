79eaf0964a9c3adbbc0e4132f21ad0484c434b95
A Chan <89209249+appe233@users.noreply.github.com>
2025-04-15 20:41:51 +0800

    Update smms_upload.sh Use Tinify to compress image

--- smms_upload.sh
+++ smms_upload.sh
@@ -1,13 +1,16 @@
-#!/bin/zsh
-# Use it with Typora + smms
-# Rename pictures' names to be their hash
-API_KEY="" # Enter your API key here
+#!/bin/bash
+
+TINIFY_API_KEY=""
+API_KEY="" # Enter the API key here.
+
+
 for image in $@
 do
     HASH_NAME=$(shasum -b -a 256 $image | awk '{ print $1 }')
-    SUFFIX=$(basename $image | grep -o "\.[a-z]\+")
-    cp $image "$HOME/$HASH_NAME$SUFFIX"
-    OUT=$(curl -s -X POST -H "Authorization: $API_KEY" -F "smfile=@$HOME/$HASH_NAME$SUFFIX" https://smms.app/api/v2/upload) && rm $HOME/$HASH_NAME$SUFFIX
-    # echo $(echo "$OUT" | jq -r ".message")
-    echo $(echo "$OUT" | /opt/homebrew/bin/jq -r ".data.url")
+
+    TINIFY_URL=$(curl -s https://api.tinify.com/shrink --user "api:$TINIFY_API_KEY" --data-binary "@$image" | /opt/homebrew/bin/jq -r ".output.url")
+    TEMP_FILE=$(mktemp)
+    curl -s $TINIFY_URL --user "api:$TINIFY_API_KEY" -H "Content-Type: application/json" --data '{ "convert": { "type": "image/webp"} }' -o $TEMP_FILE $TINIFY_URL
+    OUT=$(curl -s -X POST -H "Authorization: $API_KEY" -F "smfile=@$TEMP_FILE;type=image/webp;filename=$HASH_NAME.webp" https://sm.ms/api/v2/upload | /opt/homebrew/bin/jq -r ".data.url") 
+    echo $OUT
 done
